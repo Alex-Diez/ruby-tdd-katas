c88fd65720404c3cd3e40f7c468152a13a167da9
Alex-Diez <alex.dukhno@icloud.com>
2018-02-26 09:56:18 +0200

    bowling game kata thirteenth day

---
+++ lib/bowling/day13.rb
@@ -0,0 +1,57 @@
+class Game
+  def initialize
+    @rolls = []
+  end
+
+  def roll(pin)
+    @rolls << pin
+  end
+
+  def score
+    Enumerator
+      .new { |rolls| generate_rolls_indexes(rolls) }
+      .take(10)
+      .map { |roll| compute_frame_points(roll) }
+      .reduce(:+)
+  end
+
+  private
+
+  def compute_frame_points(roll)
+    if strike?(roll)
+      10 + strike_bonus(roll)
+    elsif spare?(roll)
+      10 + spare_bonus(roll)
+    else
+      frame_points(roll)
+    end
+  end
+
+  def generate_rolls_indexes(rolls)
+    index = 0
+    loop do
+      rolls << index
+      index += strike?(index) ? 1 : 2
+    end
+  end
+
+  def strike?(roll)
+    @rolls[roll] == 10
+  end
+
+  def strike_bonus(roll)
+    @rolls[roll + 1] + @rolls[roll + 2]
+  end
+
+  def spare_bonus(roll)
+    @rolls[roll + 2]
+  end
+
+  def spare?(roll)
+    frame_points(roll) == 10
+  end
+
+  def frame_points(roll)
+    @rolls[roll] + @rolls[roll + 1]
+  end
+end
