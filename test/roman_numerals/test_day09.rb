38bca4e6a0e41b989bff50de25aa57e5a20f5d83
Alex-Diez <alex.dukhno@icloud.com>
2018-04-05 09:06:26 +0300

    roman numerals kata ninth day

---
+++ test/roman_numerals/test_day09.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require 'roman_numerals/day09'
+
+class RomanNumeralsTest < Minitest::Test
+  def setup
+    @converter = RomanNumerals.new
+  end
+
+  def test_convert_zero
+    assert_equal '', @converter.convert(0)
+  end
+
+  def test_convert_one
+    assert_equal 'I', @converter.convert(1)
+  end
+
+  def test_convert_five
+    assert_equal 'V', @converter.convert(5)
+  end
+
+  def test_convert_two
+    assert_equal 'II', @converter.convert(2)
+  end
+
+  def test_convert_four
+    assert_equal 'IV', @converter.convert(4)
+  end
+
+  def test_convert_ten
+    assert_equal 'X', @converter.convert(10)
+  end
+
+  def test_convert_nine
+    assert_equal 'IX', @converter.convert(9)
+  end
+
+  def test_convert_twenty_nine
+    assert_equal 'XXIX', @converter.convert(29)
+  end
+end
