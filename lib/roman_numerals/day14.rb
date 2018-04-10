ad75a739318cd5a270e6f1bde263621b2d24934e
Alex-Diez <alex.dukhno@icloud.com>
2018-04-10 09:40:50 +0300

    roman numerals kata fourteenth day

---
+++ lib/roman_numerals/day14.rb
@@ -0,0 +1,16 @@
+class RomanNumerals
+  def initialize
+    @mapper =
+      Hash.new('')
+        .merge({ 1 => 'I', 5 => 'V', 10 => 'X', 9 => 'IX' })
+  end
+
+  def convert(decimal)
+    arabic = @mapper.keys.select { |k| k <= decimal }.sort.last
+    roman = @mapper[arabic]
+    if arabic
+      roman += convert(decimal - arabic)
+    end
+    roman
+  end
+end
