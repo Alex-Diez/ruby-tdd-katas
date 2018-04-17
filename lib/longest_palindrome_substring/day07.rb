2e5ae8cfac3b907c6c8ef664574b1b060d692483
Alex-Diez <alex.dukhno@icloud.com>
2018-04-17 09:55:19 +0300

    longest palindrome substring seventh day

---
+++ lib/longest_palindrome_substring/day07.rb
@@ -0,0 +1,28 @@
+class LongestPalindromeSubstring
+  def longest_substring(src)
+    first = 0
+    last = 0
+    i = 0
+    while i < src.size
+      odd_len = exapnd_around_center(src, i, i)
+      even_len = exapnd_around_center(src, i - 1, i)
+      len = [odd_len, even_len].max
+      if len > last - first
+        first = i - len / 2
+        last = i + len / 2
+      end
+      i += 1
+    end
+    src[first..last]
+  end
+
+  private
+
+  def exapnd_around_center(s, left, right)
+    while left > -1 && right < s.size && s[left] == s[right]
+      left -= 1
+      right += 1
+    end
+    right - left - 1
+  end
+end
