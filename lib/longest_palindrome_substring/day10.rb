3055ece14fe20b40bb1bdb94a1a338361d6e11bf
Alex-Diez <alex.dukhno@icloud.com>
2018-04-20 10:41:26 +0300

    longest palindrome substring tenth day

---
+++ lib/longest_palindrome_substring/day10.rb
@@ -0,0 +1,33 @@
+class LongestPalindromeSubstring
+  def longest_substring(src)
+    first = 0
+    last = 0
+    src.size.times do |i|
+      len = max_len(src, i)
+      first, last = first_last(len, i) if len > last - first
+    end
+    src[first..last]
+  end
+
+  private
+
+  def expand_around_center(s, left, right)
+    while left > -1 && right < s.size && s[left] == s[right]
+      left -= 1
+      right += 1
+    end
+    right - left - 1
+  end
+
+  def max_len(s, index)
+    odd_len = expand_around_center(s, index, index)
+    even_len = expand_around_center(s, index - 1, index)
+    [odd_len, even_len].max
+  end
+
+  def first_last(len, index)
+    first = index - len / 2
+    last = index + len / 2
+    [first, last]
+  end
+end
---
+++ test/longest_palindrome_substring/test_day10.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require 'longest_palindrome_substring/day10'
+
+class LongestPalindromeSubstringTest < Minitest::Test
+  def setup
+    @palindrome = LongestPalindromeSubstring.new
+  end
+
+  def test_single_char_palindrome
+    assert_equal 'a', @palindrome.longest_substring('a')
+  end
+
+  def test_two_chars_palindrome
+    assert_equal 'aa', @palindrome.longest_substring('aa')
+  end
+
+  def test_three_chars_palindrome
+    assert_equal 'aba', @palindrome.longest_substring('aba')
+  end
+
+  def test_four_chars_palindrome
+    assert_equal 'abba', @palindrome.longest_substring('abba')
+  end
+
+  def test_palindrome_at_the_beginning
+    assert_equal 'aba', @palindrome.longest_substring('abac')
+  end
+
+  def test_palindrome_at_the_end
+    assert_equal 'aba', @palindrome.longest_substring('sdfghjklaba')
+  end
+end
