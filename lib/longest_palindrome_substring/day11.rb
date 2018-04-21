2e36c802dc8ec2444fc4ba8bb01f39ca106b1331
Alex-Diez <alex.dukhno@icloud.com>
2018-04-21 10:36:58 +0300

    longest palindrome substring eleventh day

---
+++ lib/longest_palindrome_substring/day11.rb
@@ -0,0 +1,31 @@
+class LongestPalindromeSubstring
+  def longest_substring(src)
+    first = 0
+    last = 0
+    src.size.times do |i|
+      len = max_len(src, i)
+      first, last = first_last(len, i) if len > last - first
+    end
+    src[first..last]
+  end
+
+  private
+
+  def expand_around_center(s, left, right)
+    while left > -1 && right < s.size && s[left] == s[right]
+      left -= 1
+      right += 1
+    end
+    right - left - 1
+  end
+
+  def max_len(s, index)
+    odd_len = expand_around_center(s, index, index)
+    even_len = expand_around_center(s, index - 1, index)
+    [even_len, odd_len].max
+  end
+
+  def first_last(len, index)
+    [index - len / 2, index + len / 2]
+  end
+end
