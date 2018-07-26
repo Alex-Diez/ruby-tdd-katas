89b4d166afad9cddee0af9f46ae3a172ebf1b3c3
Alex-Diez <alex.dukhno@icloud.com>
2018-07-26 11:12:54 +0300

    k items eighth day

---
+++ test/k_items/test_day08.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'k_items/day08'
+
+describe 'k items' do
+  it 'has to filter one item from single item array' do
+    expect(k_items([1], 1)).to eq([1])
+  end
+
+  it 'has to filter one item from two items array larger is first one' do
+    expect(k_items([2, 1], 1)).to eq([2])
+  end
+
+  it 'has to filter one item from two items array larger is last one' do
+    expect(k_items([1, 2], 1)).to eq([2])
+  end
+
+  it 'has to filter one item from three items array' do
+    expect(k_items([1, 2, 3], 1)).to eq([3])
+  end
+
+  it 'has to filter two items from three items array' do
+    expect(k_items([1, 2, 3], 2)).to contain_exactly(2, 3)
+  end
+end
