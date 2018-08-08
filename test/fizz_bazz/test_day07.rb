2db4eca89c7d596da564bb8d768b8f85c379dbe5
Alex-Diez <alex.dukhno@icloud.com>
2018-08-08 10:21:09 +0300

    fizz buzz seventh day

---
+++ test/fizz_bazz/test_day07.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'fizz_buzz/day07'
+
+describe 'fizz buzz' do
+  it 'has to return empty array' do
+    expect(fizz_buzz([])).to eq([])
+  end
+
+  it 'has to return ["fizz"]' do
+    expect(fizz_buzz([3])).to eq(['fizz'])
+  end
+
+  it 'has to return ["buzz"]' do
+    expect(fizz_buzz([5])).to eq(['buzz'])
+  end
+
+  it 'has to return ["#{number}"]' do
+    expect(fizz_buzz([1])).to eq(['1'])
+  end
+
+  it 'has to return ["fizzbuzz"]' do
+    expect(fizz_buzz([15])).to eq(['fizzbuzz'])
+  end
+end
