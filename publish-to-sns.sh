c3d13d06fab55b596e9e26f57f86a3ac216f025a
Alex-Diez <alex.dukhno@icloud.com>
2018-08-12 10:31:04 +0300

    fizz buzz eleventh day

--- publish-to-sns.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-aws sns publish --topic arn:aws:sns:us-east-1:541858354261:queues-Topic-LY93DTIGPHXR --message 'hello from aws' --region us-east-1
--- send-receive-sqs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-aws sqs send-message --queue-url https://sqs.us-east-1.amazonaws.com/541858354261/queues-MessageQueue-ZBIZXEUU5YW1 --message-body 'hello sqs' --region us-east-1
-
-aws sqs receive-message --queue-url https://sqs.us-east-1.amazonaws.com/541858354261/queues-MessageQueue-ZBIZXEUU5YW1 --region us-east-1
