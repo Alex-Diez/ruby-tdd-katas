dbf7f097e295bfa0f3a7f31d2c0526d49719d2ea
Alex-Diez <alex.dukhno@icloud.com>
2018-08-16 11:14:14 +0300

    custom tap method kata first day

---
+++ test/tap_impl/test_day01.rb
@@ -0,0 +1,47 @@
+require 'rspec'
+
+class Object
+  def impl_tap(&block)
+    instance_eval(&block) if block_given?
+    self
+  end
+end
+
+describe 'tap impl method' do
+  before(:each) do
+    @local = 'self'
+  end
+
+  it 'returns self' do
+    expect(@local.impl_tap).to be @local
+  end
+
+  it 'passes self to a block' do
+    expect { |block| @local.impl_tap(&block) }.to yield_with_args(@local)
+  end
+
+  context 'access to object internals' do
+    class CustomClass
+      def initialize
+        @instance_var = 'self'
+      end
+
+      private
+        def private_method
+          @instance_var
+        end
+    end
+
+    before(:each) do
+      @object = CustomClass.new
+    end
+
+    it 'allows access to instance variable' do
+      expect { |block| @object.impl_tap { @instance_var.impl_tap(&block) } }.to yield_with_args('self')
+    end
+
+    it 'allows access to private methods' do
+      expect { |block| @object.impl_tap { private_method.impl_tap(&block) } }.to yield_with_args('self')
+    end
+  end
+end
