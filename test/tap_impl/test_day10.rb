6d419900557d2406b879a18b31163ffd5a4a5f1a
Alex-Diez <alex.dukhno@icloud.com>
2018-08-26 09:54:30 +0300

    custom tap method tenth day

---
+++ test/tap_impl/test_day10.rb
@@ -0,0 +1,42 @@
+require 'rspec'
+require 'tap_impl/day10'
+
+describe 'tap impl method' do
+  before(:each) do
+    @self = 'self'
+  end
+
+  it 'has to return self' do
+    expect(@self.tap_impl).to be @self
+  end
+
+  it 'has to pass self into a block' do
+    expect { |block| @self.tap_impl(&block) }.to yield_with_args @self
+  end
+
+  context 'access to instance internals' do
+    class CustomClass
+      def initialize(ref)
+        @instance_var = ref
+      end
+
+      private
+
+        def private_method
+          @instance_var
+        end
+    end
+
+    before(:each) do
+      @cc = CustomClass.new(@self)
+    end
+
+    it 'has to have access to instance variables' do
+      expect { |block| @cc.tap_impl { @instance_var.tap_impl(&block) } }.to yield_with_args @self
+    end
+
+    it 'has to have access to private methods' do
+      expect { |block| @cc.tap_impl { private_method.tap_impl(&block) } }.to yield_with_args @self
+    end
+  end
+end
